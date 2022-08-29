c7733b5dcb4b57078f1ae0757e67500472921b85
Peter Larson <p.larson@utexas.edu>
2022-08-29 12:17:38 -0500

    Initial Commit

---
+++ ComposableCamera/ComposableCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableCameraApp.swift
+//  ComposableCamera
+//
+//  Created by Peter Larson on 8/29/22.
+//
+
+import SwiftUI
+
+@main
+struct ComposableCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
